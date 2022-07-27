057ddb27bb4285b1a3093f77275d798efe7c2dc3
Srinivas Shanti <71806440+srinivasshanti@users.noreply.github.com>
2022-07-27 23:31:27 +0530

    Create Counting Sort 1.java

---
+++ Counting Sort 1.java
@@ -0,0 +1,35 @@
+v
+import java.util.*;
+
+
+public class Solution {
+    
+    static void printArray(int[] ar) {
+        for (int i : ar) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[100];
+        for (int i = 0; i < n; i++) {
+            ar[in.nextInt()]++;
+        
+        }
+        
+        
+        printArray(ar);
+    }
+
+}
+
+
+
+
+
+
+
+

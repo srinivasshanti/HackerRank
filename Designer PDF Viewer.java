15fd8dd46d468457e82a839b7e957b0b5ef9a915
Srinivas Shanti <71806440+srinivasshanti@users.noreply.github.com>
2022-08-14 12:16:08 +0530

    Create Designer PDF Viewer.java

---
+++ Designer PDF Viewer.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = 26;
+        int h[] = new int[n];
+        for(int i=0; i < n; i++)
+        {
+            h[i] = in.nextInt();
+        }
+        
+        String word = in.next();
+        int m=word.length();
+        int mx = 0;
+        for (int i = 0; i < m; i++) {
+            int f = h[(int) (word.charAt(i) - 'a')];  //from this we will get the number representing that particular alpahabet in array h
+            //Subtracting 'a' (or, 97) scales each letter in the alphabet to the 0-26 range that corresponds to the "buckets" in the arr array.
+            
+            if (f > mx) {
+                mx = f;
+            }
+        }
+        System.out.println((word.length() * mx));
+    }
+}
